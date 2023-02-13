7e59bc64a2c4c0cf647c872a3181e1940637ab98
Abigail <amgenz@madisoncollege.edu>
2023-02-13 12:21:22 -0600

    Added SessionFactoryProvider file to eliminate final bug

--- src/main/java/com/amgenz/persistence/UserDao.java
+++ src/main/java/com/amgenz/persistence/UserDao.java
@@ -1,13 +1,17 @@
 package com.amgenz.persistence;
 
 import com.amgenz.entity.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+
 import javax.persistence.criteria.*;
 import java.util.List;
 
 public class UserDao {
+    private final Logger logger = LogManager.getLogger(this.getClass());
     SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
     /** Returns all users from the database
      *
