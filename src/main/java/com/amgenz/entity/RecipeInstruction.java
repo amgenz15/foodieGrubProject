7861873fe2347c17fea199e42e5b5ab2c0ace448
Abigail <amgenz@madisoncollege.edu>
2023-02-18 20:01:09 -0600

    Updated recipeinstruction entity to match database, basic javabean

--- src/main/java/com/amgenz/entity/RecipeInstruction.java
+++ src/main/java/com/amgenz/entity/RecipeInstruction.java
@@ -18,10 +18,86 @@ public class RecipeInstruction {
     @ManyToOne
     private Recipe recipe;
 
-    @Column(name = "instruction_1")
-    private String instruction1;
+    @Column(name = "instruction")
+    private String instruction;
 
+    /**
+     * Instantiates a new Recipe instruction.
+     */
+    public RecipeInstruction() {
+    }
 
+    /**
+     * Instantiates a new Recipe instruction.
+     *
+     * @param recipe      the recipe
+     * @param instruction the instruction
+     */
+    public RecipeInstruction(Recipe recipe, String instruction) {
+        this.recipe = recipe;
+        this.instruction = instruction;
+    }
 
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
 
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets recipe.
+     *
+     * @return the recipe
+     */
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    /**
+     * Sets recipe.
+     *
+     * @param recipe the recipe
+     */
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    /**
+     * Gets instruction.
+     *
+     * @return the instruction
+     */
+    public String getInstruction() {
+        return instruction;
+    }
+
+    /**
+     * Sets instruction.
+     *
+     * @param instruction the instruction
+     */
+    public void setInstruction(String instruction) {
+        this.instruction = instruction;
+    }
+
+    @Override
+    public String toString() {
+        return "RecipeInstruction{" +
+                "id=" + id +
+                ", recipe=" + recipe +
+                ", instruction='" + instruction + '\'' +
+                '}';
+    }
 }
