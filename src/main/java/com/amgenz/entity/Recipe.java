1308bb58384d05e5fa93eed79ae39a0ed36ed317
Abigail <amgenz@madisoncollege.edu>
2023-02-18 19:53:14 -0600

    Updatd Recipe entity to match new database, basic javabean created

--- src/main/java/com/amgenz/entity/Recipe.java
+++ src/main/java/com/amgenz/entity/Recipe.java
@@ -16,10 +16,23 @@ public class Recipe {
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
     @GenericGenerator(name = "native", strategy = "native")
     private int id;
-    @Column(name = "recipe_title")
+    @Column(name = "recipe_name")
     private String recipeName;
     @ManyToOne
     private User user;
+
+    private int calories;
+
+    private int protein;
+
+    private int carbohydrates;
+
+    private int fat;
+
+    @Column(name = "total_time_in_min")
+    private int totalTimeInMin;
+
+    private String type;
 
     @OneToMany(mappedBy = "recipes", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
     private Set<RecipeInstruction> instructions = new HashSet<>();
@@ -27,6 +40,105 @@ public class Recipe {
     @OneToMany(mappedBy = "recipes", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
     private Set<RecipeIngredient> ingredients = new HashSet<>();
 
+    public Recipe() {
+    }
 
+    public Recipe(String recipeName, User user, int calories, int protein, int carbohydrates, int fat,
+                  int totalTimeInMin, String type) {
+        this.recipeName = recipeName;
+        this.user = user;
+        this.calories = calories;
+        this.protein = protein;
+        this.carbohydrates = carbohydrates;
+        this.fat = fat;
+        this.totalTimeInMin = totalTimeInMin;
+        this.type = type;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public int getProtein() {
+        return protein;
+    }
+
+    public void setProtein(int protein) {
+        this.protein = protein;
+    }
+
+    public int getCarbohydrates() {
+        return carbohydrates;
+    }
+
+    public void setCarbohydrates(int carbohydrates) {
+        this.carbohydrates = carbohydrates;
+    }
+
+    public int getFat() {
+        return fat;
+    }
+
+    public void setFat(int fat) {
+        this.fat = fat;
+    }
+
+    public int getTotalTimeInMin() {
+        return totalTimeInMin;
+    }
+
+    public void setTotalTimeInMin(int totalTimeInMin) {
+        this.totalTimeInMin = totalTimeInMin;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Recipe{" +
+                "id=" + id +
+                ", recipeName='" + recipeName + '\'' +
+                ", user=" + user +
+                ", calories=" + calories +
+                ", protein=" + protein +
+                ", carbohydrates=" + carbohydrates +
+                ", fat=" + fat +
+                ", totalTimeInMin=" + totalTimeInMin +
+                ", type='" + type + '\'' +
+                '}';
+    }
 }
