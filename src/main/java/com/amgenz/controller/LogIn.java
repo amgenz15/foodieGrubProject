c94299d789d650662a007c336bce070bd175d9ed
Abigail Genz <amgenz@madisoncollege.edu>
2023-03-23 15:12:00 -0500

    Added and update pom.xml to allow incognito files

--- src/main/java/com/amgenz/controller/LogIn.java
+++ src/main/java/com/amgenz/controller/LogIn.java
@@ -1,6 +1,6 @@
 package com.amgenz.controller;
 
-import edu.matc.util.PropertiesLoader;
+import com.amgenz.util.PropertiesLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
